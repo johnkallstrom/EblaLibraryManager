9eedc4f94b461787b58541abc2a6ce67382b0de0
John Källström <jekallstrom@gmail.com>
2021-04-06 12:59:53 +0200

    Add tests for settingsviewmodel validator

--- EblaLibraryManager.Tests/Validators/SettingsViewModelValidatorTests.cs
+++ EblaLibraryManager.Tests/Validators/SettingsViewModelValidatorTests.cs
@@ -17,6 +17,26 @@ namespace EblaLibraryManager.Tests.Validators
         }
 
         [Test]
+        public void ShouldReturnErrorMessage_WhenPhoneNumberRegexValidationFails()
+        {
+            var model = new SettingsViewModel();
+            model.PhoneNumber = "notvalidphonenumber";
+
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(model => model.PhoneNumber).WithErrorMessage("The phone number you entered is not valid.");
+        }
+
+        [Test]
+        public void ShouldNotReturnErrorMessage_WhenPhoneNumberRegexValidationSucceeded()
+        {
+            var model = new SettingsViewModel();
+            model.PhoneNumber = "08-1234567";
+
+            var result = _validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(model => model.PhoneNumber);
+        }
+
+        [Test]
         public void ShouldReturnErrorMessage_WhenEmailRegexValidationFails()
         {
             var model = new SettingsViewModel();
