847a4e27e47f5acd6b3cc08a5a57fe5257ead64d
John Källström <jekallstrom@gmail.com>
2021-04-12 15:39:00 +0200

    Add new rules to create and edit book validators

--- EblaLibraryManager.Web/Validators/CreateBookViewModelValidator.cs
+++ EblaLibraryManager.Web/Validators/CreateBookViewModelValidator.cs
@@ -13,11 +13,14 @@ namespace EblaLibraryManager.Web.Validators
                 .MaximumLength(50).WithMessage("The title cannot be longer than 50 characters.");
 
             RuleFor(model => model.Language)
-                .NotNull().WithMessage("Please choose a language.")
-                .NotEmpty().WithMessage("Please choose a language.");
+                .NotNull().WithMessage("Please choose language.")
+                .NotEmpty().WithMessage("Please choose language.");
 
             RuleFor(model => model.GenreId)
-                .NotEmpty().WithMessage("Please choose a genre.");
+                .NotEmpty().WithMessage("Please choose genre.");
+
+            RuleFor(model => model.AuthorId)
+                .NotEmpty().WithMessage("Please choose author.");
 
             RuleFor(model => model.Publisher)
                 .MaximumLength(50).WithMessage("The publisher cannot be longer than 50 characters.");
---
+++ EblaLibraryManager.Web/Validators/EditBookViewModelValidator.cs
@@ -0,0 +1,17 @@
+﻿using EblaLibraryManager.Web.ViewModels.Book;
+using FluentValidation;
+
+namespace EblaLibraryManager.Web.Validators
+{
+    public class EditBookViewModelValidator : AbstractValidator<EditBookViewModel>
+    {
+        public EditBookViewModelValidator()
+        {
+            RuleFor(model => model.Title)
+                .MaximumLength(50).WithMessage("The title cannot be longer than 50 characters.");
+
+            RuleFor(model => model.Publisher)
+                .MaximumLength(50).WithMessage("The publisher cannot be longer than 50 characters.");
+        }
+    }
+}
