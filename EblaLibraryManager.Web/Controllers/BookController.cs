0e0fc5584aedb1ff8acec7cecd87c865dfc9efcf
John Källström <jekallstrom@gmail.com>
2021-04-12 15:40:22 +0200

    Add create and edit book action methods

--- EblaLibraryManager.Web/Controllers/BookController.cs
+++ EblaLibraryManager.Web/Controllers/BookController.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using EblaLibraryManager.Core.Parameters;
 using EblaLibraryManager.Core.Services.Interfaces;
+using EblaLibraryManager.Data.Models;
 using EblaLibraryManager.Web.ViewModels.Book;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,15 +16,18 @@ namespace EblaLibraryManager.Web.Controllers
     public class BookController : Controller
     {
         private readonly IMapper _mapper;
+        private readonly IAuthorService _authorService;
         private readonly IGenreService _genreService;
         private readonly IBookService _bookService;
 
         public BookController(
             IMapper mapper,
+            IAuthorService authorService,
             IGenreService genreService,
             IBookService bookService)
         {
             _mapper = mapper;
+            _authorService = authorService;
             _genreService = genreService;
             _bookService = bookService;
         }
@@ -54,18 +59,32 @@ namespace EblaLibraryManager.Web.Controllers
         {
             var model = new CreateBookViewModel();
 
-            model.GenreOptions = await GetGenreOptions();
+            model.GenreOptions = await GetGenreSelectOptions();
+            model.AuthorOptions = await GetAuthorSelectOptions();
 
             return View(model);
         }
 
         [HttpPost]
         [Authorize(Roles = "Librarian")]
-        public IActionResult CreateBook(CreateBookViewModel model)
+        public async Task<IActionResult> CreateBook(CreateBookViewModel model)
         {
             if (!ModelState.IsValid) return View(model);
 
-            return View();
+            try
+            {
+                var book = _mapper.Map<Book>(model);
+
+                await _bookService.CreateBookAsync(book);
+
+                return RedirectToAction(nameof(BookController.Index), "Book");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            return View(model);
         }
 
         [HttpGet]
@@ -79,14 +98,73 @@ namespace EblaLibraryManager.Web.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        [Authorize(Roles = "Librarian")]
+        public async Task<IActionResult> EditBook(int bookId)
+        {
+            var book = await _bookService.GetBookByIdAsync(bookId);
+
+            var model = _mapper.Map<EditBookViewModel>(book);
+            model.GenreOptions = await GetGenreSelectOptions();
+            model.AuthorOptions = await GetAuthorSelectOptions();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Librarian")]
+        public async Task<IActionResult> EditBook(EditBookViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            try
+            {
+                var book = await _bookService.GetBookByIdAsync(model.BookId);
+
+                _mapper.Map(model, book);
+                
+                _bookService.UpdateBook(book);
+
+                return RedirectToAction(nameof(BookController.Index), "Book");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Librarian")]
+        public async Task<IActionResult> DeleteBook(int bookId)
+        {
+            var book = await _bookService.GetBookByIdAsync(bookId);
+            
+            _bookService.DeleteBook(book);
+
+            var bookList = await _bookService.GetBooksAsync();
+            
+            return ViewComponent("BookTable", _mapper.Map<IEnumerable<BookSlimViewModel>>(bookList));
+        }
+
         #region Private Methods
-        private async Task<List<SelectListItem>> GetGenreOptions()
+        private async Task<List<SelectListItem>> GetAuthorSelectOptions()
+        {
+            var authors = await _authorService.GetAuthorsAsync();
+            var authorOptions = new List<SelectListItem>();
+
+            authorOptions = authors.Select(g => new SelectListItem(g.Name, g.Id.ToString())).OrderBy(i => i.Text).ToList();
+
+            return authorOptions;
+        }
+
+        private async Task<List<SelectListItem>> GetGenreSelectOptions()
         {
             var genres = await _genreService.GetGenresAsync();
             var genreOptions = new List<SelectListItem>();
 
             genreOptions = genres.Select(g => new SelectListItem(g.Name, g.Id.ToString())).OrderBy(i => i.Text).ToList();
-            genreOptions.Insert(0, new SelectListItem("Choose genre", "0", true, true));
 
             return genreOptions;
         }
