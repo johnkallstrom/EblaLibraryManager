d906b18598247d1570b7e4b8f2c3dc28b85e05b8
John Källström <jekallstrom@gmail.com>
2021-04-13 21:13:12 +0200

    Refactored view models

--- EblaLibraryManager.Web/ViewModels/Book/BookSlimViewModel.cs
+++ EblaLibraryManager.Web/ViewModels/Book/BookSlimViewModel.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Status { get; set; }
         public AuthorSlimViewModel Author { get; set; }
         public string Genre { get; set; }
         public int TotalPages { get; set; }
-        public AvailabilityStatusViewModel AvailabilityStatus { get; set; }
     }
 }
--- EblaLibraryManager.Web/ViewModels/Book/BookViewModel.cs
+++ EblaLibraryManager.Web/ViewModels/Book/BookViewModel.cs
@@ -8,11 +8,11 @@ namespace EblaLibraryManager.Web.ViewModels.Book
         public string Title { get; set; }
         public string Description { get; set; }
         public string Language { get; set; }
+        public string Status { get; set; }
         public string Publisher { get; set; }
         public int TotalPages { get; set; }
-        public DateTime? Borrowed { get; set; }
+        public DateTime? Borrowed { get; set; } 
         public DateTime? DueDate { get; set; }
-        public AvailabilityStatusViewModel AvailabilityStatus { get; set; }
         public AuthorViewModel Author { get; set; }
         public GenreViewModel Genre { get; set; }
     }
