10d3979b9fb1dd9567e2438054c85d1bdd934c3a
John Källström <jekallstrom@gmail.com>
2021-04-13 21:13:40 +0200

    Add new lending controller

---
+++ EblaLibraryManager.Web/Controllers/LendingController.cs
@@ -0,0 +1,39 @@
+﻿using EblaLibraryManager.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EblaLibraryManager.Web.Controllers
+{
+    public class LendingController : Controller
+    {
+        private readonly ILendingService _lendingService;
+        private readonly IIdentityService _identityService;
+
+        public LendingController(
+            ILendingService lendingService,
+            IIdentityService identityService)
+        {
+            _lendingService = lendingService;
+            _identityService = identityService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task CreateLending(int bookId)
+        {
+            var user = await _identityService.GetCurrentUserAsync();
+
+            if (user is not null)
+            {
+                await _lendingService.CreateLendingAsync(bookId, user);
+                ViewBag.DisplayMessage = true;
+            }
+        }
+    }
+}
