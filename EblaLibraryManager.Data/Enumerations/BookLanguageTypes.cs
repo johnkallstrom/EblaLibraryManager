b310f2fe0df196fd2f1b3ccce2e42cefd7465c0a
John Källström <jekallstrom@gmail.com>
2021-04-13 21:10:17 +0200

    Add enumerations

---
+++ EblaLibraryManager.Data/Enumerations/BookLanguageTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace EblaLibraryManager.Data.Enumerations
+{
+    public static class BookLanguageTypes
+    {
+        public const string Arabic = "Arabic";
+        public const string Chinese = "Chinese";
+        public const string English = "English";
+        public const string French = "French";
+        public const string German = "German";
+        public const string Italian = "Italian";
+        public const string Russian = "Russian";
+        public const string Swedish = "Swedish";
+        public const string Spanish = "Spanish";
+    }
+}
---
+++ EblaLibraryManager.Data/Enumerations/BookStatusTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace EblaLibraryManager.Data.Enumerations
+{
+    public static class BookStatusTypes
+    {
+        public const string Available = "Available";
+        public const string Reserved = "Reserved";
+        public const string Loaned = "Loaned";
+        public const string None = "None";
+    }
+}
