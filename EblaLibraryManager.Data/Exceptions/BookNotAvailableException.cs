197f744b80de3011d2a8cca0a1b4c40e38396f6b
John Källström <jekallstrom@gmail.com>
2021-04-13 21:10:31 +0200

    Add custom book exception

---
+++ EblaLibraryManager.Data/Exceptions/BookNotAvailableException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EblaLibraryManager.Data.Exceptions
+{
+    public class BookNotAvailableException : Exception
+    {
+        public BookNotAvailableException()
+        {
+        }
+
+        public BookNotAvailableException(string message) : base(message)
+        {
+        }
+
+        public BookNotAvailableException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
